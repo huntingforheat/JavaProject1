bde5f2170265585cc676652fcb63168ccec27604
huntingforheat <asktt1234@gmail.com>
2024-03-07 17:14:18 +0900

    240307 퀴즈

---
+++ FirstJava/src/quiz/quiz_01.java
@@ -0,0 +1,21 @@
+package quiz;
+
+public class quiz_01 {
+
+	public static void main(String[] args) {
+		System.out.println("==================\n");
+		System.out.println("       /) /)      ");
+		System.out.println("      (  ..)      ");
+		System.out.println("      (  >♡       ");
+		System.out.println(" Have a Good Time.\n");
+		System.out.println("==================\n");
+		
+		System.out.println("    #### 회비정보 ####    \n");
+		System.out.println("===========================      ");
+		
+		
+		
+
+	}
+
+}
---
+++ FirstJava/src/quiz/quiz_02.java
@@ -0,0 +1,14 @@
+package quiz;
+
+public class quiz_02 {
+
+	public static void main(String[] args) {
+		// 11개 지하철역 지나는데 출발역에서 도착역까지 37분 소요 소수점 두자리까지
+		
+		double a = 37;
+		double b = 11;
+		double result = a / b;
+		System.out.printf("한 역에 걸리는 시간 : " + String.format("%.2f", result));
+	}
+
+}
